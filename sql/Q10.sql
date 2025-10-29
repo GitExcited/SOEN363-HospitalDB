6d45575ee04adb4cf18ded54b6c8d0f39233b9e8
aribertobello <aribertobelloveras@gmail.com>
2025-10-29 15:23:08 -0400

    removed duplicate queery

--- sql/Q10.sql
+++ sql/Q10.sql
@@ -1,10 +1,3 @@
-SELECT * 
-FROM noteevents n
-WHERE n.category = 'Discharge summary'
-LIMIT 10;
-
-
-/*alternative */
 SELECT n.chartdate, n.storetime, n.category, n.description, n."text" 
 FROM noteevents n
 WHERE n.category = 'Discharge summary'
